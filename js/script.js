e8128d05700bf5713dd8994a6e62d037d4f4b71c
Sohaib Nisar <sohaib39826@gmail.com>
2019-08-10 13:55:23 +0500

    Sohaib Nisar

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
     var content = $(".paralex-container"); 
     $(window).scroll(function(){
         var abc = $(window).scrollTop();
-        $("#main").css("background-position-y",+(-abc*0.2)+"px");
+        $("#main").css("background-position-y",+(-abc*0.3)+"px");
         $(".main_contian").css("position","relative").css("top",+(abc*0.5+130)+"px");
         $(".main_contian2").css("top",+(abc*0.5+500)+"px");
         if($(window).scrollTop() <= 40){
             navbar.removeClass('navbar-scroll');
-        } else {
+        } 
+        else if($(window).scrollTop() >= 40 || window.matchMedia("(max-width: 768px)").matches){
             navbar.addClass('navbar-scroll');
         }
     });
